3ff0f0a85388c5993c2333b62ac66e18ad17072b
rtsh13 <ruchitsingh13@gmail.com>
2024-07-20 19:53:52 +0530

    add protocol and scan custom errors

--- error.go
+++ error.go
@@ -18,3 +18,19 @@ type UnknownCommand struct {
 func (u UnknownCommand) Error() string {
 	return fmt.Sprintf("(error) ERR unknown command '%v', with args beginning with: %v", u.Command, u.Args)
 }
+
+type ProtocolError struct {
+	Message string
+}
+
+func (p ProtocolError) Error() string {
+	return fmt.Sprintf("(error) ERR Protocol error: %s", p.Message)
+}
+
+type ScannerError struct {
+	Message string
+}
+
+func (s ScannerError) Error() string {
+	return fmt.Sprintf("(error) ERR Input Scan error: %s", s.Message)
+}
