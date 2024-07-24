02f8bd8e6ad28c20fc46b386fab31e67f7ee61a9
rtsh13 <ruchitsingh13@gmail.com>
2024-07-24 20:40:01 +0530

    server execution and database ops router

---
+++ server/exec.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"strings"
+
+	pkg "github.com/zenith"
+	resp "github.com/zenith/redis-protocol"
+)
+
+type Server interface {
+	Exec(input string) interface{}
+}
+
+type server struct {
+	protocol resp.Protocol
+	Database dbOps
+}
+
+func New() Server {
+	protocol := resp.New()
+	d := newDatabase()
+
+	return &server{protocol: protocol, Database: d}
+}
+
+type respond func(string, error) interface{}
+
+func dbSerializer(protocol resp.Protocol) respond {
+	return func(response string, err error) interface{} {
+		if err != nil {
+			return protocol.Serialize([]string{err.Error()})
+		}
+
+		return protocol.Serialize([]string{response})
+	}
+}
+
+func (s *server) Exec(input string) interface{} {
+	responder := dbSerializer(s.protocol)
+
+	instructions, err := s.protocol.Deserialize(input)
+	if err != nil {
+		return responder("", err)
+	}
+
+	cmd := strings.Split(instructions.String(), " ")
+
+	switch cmd[0] {
+	case pkg.SetCMD:
+		s.Database.Set(cmd[1], cmd[2])
+		return responder("OK", nil)
+	case pkg.GetCMD:
+		return responder(s.Database.Get(cmd[1]), nil)
+	case pkg.DelCMD:
+		s.Database.Delete(cmd[1])
+		return responder("OK", nil)
+	case pkg.EchoCMD:
+		return responder(s.Database.Echo(cmd[1]), nil)
+	case pkg.PingCMD:
+		return responder(s.Database.Ping(), nil)
+	default:
+		return responder("", pkg.UnknownCommand{Command: cmd[0], Args: cmd[1:]})
+	}
+}
