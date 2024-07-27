5bd99423e07c62b3f4658be2597c1b645a03d8b1
rtsh13 <ruchitsingh13@gmail.com>
2024-07-27 22:03:03 +0530

    add server listener

--- internal/server/main.go
+++ internal/server/main.go
@@ -1,34 +1,10 @@
 package main
 
 import (
-	"fmt"
-
-	resp "github.com/zenith/redis-protocol"
 	"github.com/zenith/server"
 )
 
 func main() {
 	s := server.New()
-
-	r := resp.New()
-
-	response := s.Exec("+PING\r\n")
-	ss, _ := r.Deserialize(response)
-	fmt.Print(ss.String())
-
-	response = s.Exec("*3\r\n$3\r\nSET\r\n$5\r\nHELLO\r\n$5\r\nWORLD\r\n ")
-	ss, _ = r.Deserialize(response)
-	fmt.Print(ss.String())
-
-	response = s.Exec("*2\r\n$3\r\nGET\r\n$5\r\nHELaL\r\n")
-	ss, _ = r.Deserialize(response)
-	fmt.Print(ss.String())
-
-	response = s.Exec("*2\r\n$3\r\nDEL\r\n$5\r\nHELLO\r\n")
-	ss, _ = r.Deserialize(response)
-	fmt.Print(ss.String())
-
-	response = s.Exec("*2\r\n$4\r\nECHO\r\n$5\r\nHELLO\r\n")
-	ss, _ = r.Deserialize(response)
-	fmt.Print(ss.String())
+	s.Listen()
 }
